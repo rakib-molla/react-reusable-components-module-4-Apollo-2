1929dc9e0ee9197c3b56c8a1b73cc1eaa41a443a
rakib-molla <rakibmail1997@gmail.com>
2024-01-24 10:47:20 +0600

    portal add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 // import MainLayout from "./components/layout/MainLayout";
+import { useState } from "react";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
+import Modal from "./components/ui/Modal";
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const handleOpenModalClose=()=>{
+    setModal(prev=> !prev);
+  }
   return (
     <Container>
-      <div className="h-screen flex items-center justify-center ">
+      <div className="h-screen w-full flex items-center justify-center ">
         
         {/* reUseable button  */}
-         <Button>open modal</Button>
+         <Button onClick={()=> setModal((prev)=> !prev)}>open modal</Button>
+         <Modal isOpen={modal} onClose={handleOpenModalClose}> 
+         <h1>this is modal </h1>
+         </Modal>
       </div>
     </Container>
   );
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,11 +1,17 @@
+import { createPortal } from "react-dom";
+import cn from "../../utils/cn";
 
-
-const Modal = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+const Modal = ({ isOpen, onClose, children }) => {
+  return createPortal(
+    <div
+      className={cn("fixed inset-0 bg-gray-700/70 invisible z-[999]", {
+        visible: isOpen,
+      })}
+    >
+      <div className="bg-white"> {children}</div>
+    </div>,
+    document.getElementById("portal") as Element
+  );
 };
 
 export default Modal;
