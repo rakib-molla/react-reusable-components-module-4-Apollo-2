762ef5c62d3e8f96797735b9496175eb0af6f524
rakib-molla <rakibmail1997@gmail.com>
2024-01-24 11:05:04 +0600

    outsideclick close modal

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import Modal from "./components/ui/Modal";
 
 function App() {
   const [modal, setModal] = useState(false);
-  const handleOpenModalClose=()=>{
-    setModal(prev=> !prev);
-  }
+  const handleOpenModalClose = () => {
+    setModal((prev) => !prev);
+  };
   return (
     <Container>
       <div className="h-screen w-full flex items-center justify-center ">
-        
         {/* reUseable button  */}
-         <Button onClick={()=> setModal((prev)=> !prev)}>open modal</Button>
-         <Modal isOpen={modal} onClose={handleOpenModalClose}> 
-         <h1>this is modal </h1>
-         </Modal>
+        <Button onClick={() => setModal((prev) => !prev)}>open modal</Button>
+        <Modal isOpen={modal} onClose={handleOpenModalClose}>
+          <Modal.CloseButton />
+          <h1>this is modal </h1>
+        </Modal>
       </div>
     </Container>
   );
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,17 +1,38 @@
 import { createPortal } from "react-dom";
 import cn from "../../utils/cn";
+import {MouseEvent, useRef } from "react";
 
 const Modal = ({ isOpen, onClose, children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClose = (e: MouseEvent) => {
+    if (!containerRef?.current?.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
   return createPortal(
     <div
-      className={cn("fixed inset-0 bg-gray-700/70 invisible z-[999]", {
-        visible: isOpen,
-      })}
+      className={cn(
+        "fixed flex items-center justify-center inset-0 bg-gray-700/70 invisible z-[999]",
+        {
+          visible: isOpen,
+        }
+      )}
+      onClick={handleOutsideClose}
     >
-      <div className="bg-white"> {children}</div>
+      <div ref={containerRef} className="bg-white max-w-sm rounded-md">
+        {" "}
+        {children}
+      </div>
     </div>,
     document.getElementById("portal") as Element
   );
 };
+
+const closeButton = ()=>{
+   return <div>Close Button</div>
+}
+Modal.CloseButton = closeButton;
 
 export default Modal;
